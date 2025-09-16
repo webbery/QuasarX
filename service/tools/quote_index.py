d5d553e30abe38abeb3a7db6c2a3e320d9212812
yuanwenbin <yuanwenbin@kuandeng.com>
2025-09-16 10:32:07 +0800

    update test and python script

--- service/tools/quote_index.py
+++ service/tools/quote_index.py
@@ -7,13 +7,13 @@ def get_index_rt_value(index):
     if index % 2 == 0:
         data = ak.stock_zh_index_spot_em('沪深重要指数')
         for _, row in data.iterrows():
-            if row['代码'] in ['000300', '000016']:
+            if row['代码'] in ['000001', '399001', '899050', '399006']:
                 rows.append([row['代码'], row['最新价'], row['涨跌幅']])
-    else:
-        data = ak.stock_zh_index_spot_sina()
-        for _, row in data.iterrows():
-            if row['代码'] in ['sh000300', 'sh000016']:
-                rows.append([row['代码'], row['最新价'], row['涨跌幅']])
+    # else:
+    #     data = ak.stock_zh_index_spot_sina()
+    #     for _, row in data.iterrows():
+    #         if row['代码'] in ['sh000001', 'sz399001']:
+    #             rows.append([row['代码'], row['最新价'], row['涨跌幅']])
 
     return pd.DataFrame(data=rows)
 
