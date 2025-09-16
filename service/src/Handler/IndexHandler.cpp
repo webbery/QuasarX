8cc894b67a080067f9447717da89a7672d2b11c0
yuanwenbin <yuanwenbin@kuandeng.com>
2025-09-16 10:32:07 +0800

    update ci

--- service/src/Handler/IndexHandler.cpp
+++ service/src/Handler/IndexHandler.cpp
@@ -1,8 +1,10 @@
 #include "Handler/IndexHandler.h"
+#include "Util/string_algorithm.h"
 #include "Util/system.h"
 #include "json.hpp"
 #include "Bridge/exchange.h"
 #include "server.h"
+#include "std_header.h"
 #include <string>
 
 IndexHandler::IndexHandler(Server* server): HttpHandler(server),_times(0) {}
@@ -11,7 +13,22 @@ void IndexHandler::get(const httplib::Request& req, httplib::Response& res) {
     String cmd = "python tool/quote_index.py " + std::to_string(_times++);
     String output;
     RunCommand(cmd, output);
+    Vector<String> lines;
+    split(output, lines, "\n");
+    if (lines.size() == 1) {
+        res.status = 400;
+        return;
+    }
+
     nlohmann::json jsn;
-    jsn[""] = "";
+    for (int i = 1; i < lines.size(); ++i) {
+        Vector<String> cols;
+        split(lines[i], cols, " ");
+        nlohmann::json info;
+        info["code"] = cols[1];
+        info["price"] = cols[2];
+        info["rate"] = cols[3];
+        jsn.emplace_back(std::move(info));
+    }
     res.set_content(jsn.dump(), "application/json");
 }
